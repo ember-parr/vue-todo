d6b28710572178bfb61c7330f93ae3a17dc244cd
Ember P <ember-devops@gmail.com>
2021-03-03 10:50:14 -0600

    basics added to ToDoList file

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Hey, Ember.... Do This Stuff Today.</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    h2 {
+        color: teal;
+    }
+</style>
