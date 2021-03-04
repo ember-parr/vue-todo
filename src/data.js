1180cd4cff8707b69a8ff0afbcfdaaac14f69b29
Ember P <ember-devops@gmail.com>
2021-03-03 18:32:44 -0600

    populated dummy data

--- src/data.js
+++ src/data.js
@@ -26,37 +26,102 @@ export const todoItems = [
   },
   {
     id: 6,
-    name: "dance dance revolution",
-    complete: true
+    name: "make a smoothie",
+    complete: false
   },
   {
     id: 7,
-    name: "dance dance revolution",
+    name: "nap",
     complete: true
   },
   {
     id: 8,
-    name: "dance dance revolution",
-    complete: true
+    name: "modify nerf gun",
+    complete: false
   },
   {
     id: 9,
-    name: "dance dance revolution",
+    name: "ACTUALLY clean your room",
     complete: true
   },
   {
     id: 10,
-    name: "dance dance revolution",
-    complete: true
+    name: "eat a kiwi",
+    complete: false
   },
   {
     id: 11,
-    name: "dance dance revolution",
+    name: "Make an awesome paper airplaine",
     complete: true
   },
   {
     id: 12,
-    name: "dance dance revolution",
+    name: "Run around your house 3 times",
+    complete: true
+  },
+  {
+    id: 13,
+    name: "Do the Rubix Cube",
+    complete: true
+  },
+  {
+    id: 14,
+    name: "Staring Contest with a lizard",
+    complete: true
+  },
+  {
+    id: 15,
+    name: "Destroy a sheet of bubble wrap",
+    complete: false
+  },
+  {
+    id: 16,
+    name: "Reclick the button",
+    complete: false
+  },
+  {
+    id: 17,
+    name: "pull weeds from neighbors garden",
+    complete: false
+  },
+  {
+    id: 18,
+    name: "Contemplate life",
+    complete: false
+  },
+  {
+    id: 19,
+    name: "Learn how to play solitaire",
+    complete: false
+  },
+  {
+    id: 20,
+    name: "Play solitaire",
+    complete: false
+  },
+  {
+    id: 21,
+    name: "empty amazon 'save for later' section.... it's ridiculous",
+    complete: true
+  },
+  {
+    id: 22,
+    name: "fluff pillows",
+    complete: true
+  },
+  {
+    id: 23,
+    name: "wash socks",
+    complete: true
+  },
+  {
+    id: 24,
+    name: "ride scooter around the block",
+    complete: true
+  },
+  {
+    id: 25,
+    name: "play a round of BOP-IT",
     complete: true
   }
 ];
